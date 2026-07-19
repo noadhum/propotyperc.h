5df86c37f4c51d55ae1a5c648b5f2f4126f36125
noadhum <noa.adhum99@gmail.com>
2026-07-19 13:12:08 +0700

    added Car, Car_Config, and car_ functions

--- propotype.h
+++ propotype.h
@@ -31,6 +31,7 @@
 #endif
 
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
@@ -42,7 +43,7 @@
 
 PROPODEF void propo_panic(const char *msg)
 {
-     fprintf(stderr, msg);
+     fprintf(stderr, "%s\n", msg);
      abort();
 }
 
@@ -54,6 +55,16 @@ PROPODEF void propo_panicf(const char *fmt, ...)
      va_end(args);
      fprintf(stderr, "\n");
      abort();
+}
+
+static int propo_max(int a, int b)
+{
+     return (a > b) ? a : b;
+}
+
+static int propo_min(int a, int b)
+{
+     return (a < b) ? a : b;
 }
 
 #ifndef PROPO_BUFFER_SIZE
@@ -71,12 +82,33 @@ static void buf_append_byte(Buffer *buffer, unsigned char byte)
      buffer->data[buffer->count++] = byte;
 }
 
+/*
+ * This function only accepts number around 0-9999
+ */
+static void buf_append_int(Buffer *buffer, int number)
+{
+     int thousand = (number / 1000) % 10;
+     int hundred = (number / 100) % 10;
+     int ten = (number / 10) % 10;
+     int one = (number / 1) % 10;
+
+     buf_append_byte(buffer, thousand + '0');
+     buf_append_byte(buffer, hundred + '0');
+     buf_append_byte(buffer, ten + '0');
+     buf_append_byte(buffer, one + '0');
+}
+
+static void buf_append_cstr(Buffer *buffer, const char *cstr)
+{
+     for (const unsigned char *ucstr = (const unsigned char *)cstr; *ucstr != '\0'; ucstr++) {
+          buf_append_byte(buffer, *ucstr);
+     }
+}
+
 static Buffer buf_from_cstr(const char *cstr)
 {
      Buffer buffer = {0};
-     for (const unsigned char *ucstr = (const unsigned char *)cstr; *ucstr != '\0'; ucstr++) {
-          buf_append_byte(&buffer, *ucstr);
-     }
+     buf_append_cstr(&buffer, cstr);
 
      return buffer;
 }
@@ -131,7 +163,7 @@ PROPODEF void propotype_cleanup(void)
 #    define SOCKET_INVALID (-1)
 #    define SOCKET_ERROR (-1)
 #    define SOCKET_GET_ERROR() errno
-#    define SOCKET_PRINT_ERROR(err) fprintf(stderr, "%s (%d): ", __func__, err); fprintf(stderr, strerror(err)); fprintf(stderr, "\n")
+#    define SOCKET_PRINT_ERROR(err) fprintf(stderr, "%s (%d): ", __func__, err); fprintf(stderr, "%s\n", strerror(err));
 
 #    define SOCKET_CREATE() (socket(AF_INET, SOCK_DGRAM, 0))
 #    define SOCKET_CLOSE(socket) (close(socket))
@@ -228,6 +260,147 @@ PROPODEF int client_send_with_cstr(const Client *client, const char *cstr)
 {
      Buffer buffer = buf_from_cstr(cstr);
      return socket_send(client->socket, &buffer, &client->address);
+}
+
+typedef struct {
+     // Steering
+     bool steering_auto_return;
+     bool steering_reverse;
+     int steering_limits[3];
+     size_t steering_limits_idx;
+     // Throttle
+     bool throttle_auto_return;
+     bool throttle_reverse;
+     int throttle_limits[3];
+     size_t throttle_limits_idx;
+} Car_Config;
+
+#define PROPO_DEFAULT_CAR_CONFIG                        \
+     ((Car_Config){                                     \
+          .steering_auto_return = true,                 \
+          .steering_reverse = false,                    \
+          .steering_limits = {200, 300, 500},           \
+          .steering_limits_idx = 1,                     \
+                                                        \
+          .throttle_auto_return = true,                 \
+          .throttle_reverse = false,                    \
+          .throttle_limits = {150, 300, 500},           \
+          .throttle_limits_idx = 2                      \
+     })
+
+#ifndef PROPO_CAR_NEUTRAL_VALUE
+#define PROPO_CAR_NEUTRAL_VALUE 1500
+#endif // PROPO_CAR_NEUTRAL_VALUE
+
+// Car commands (ch0)
+const char PROPO_CAR_COMMAND_L[]             = "1250";
+const char PROPO_CAR_COMMAND_R[]             = "1900";
+const char PROPO_CAR_COMMAND_LIGHTOFF[]      = "1200";
+const char PROPO_CAR_COMMAND_CABINLIGHT[]    = "1350";
+const char PROPO_CAR_COMMAND_HEADLIGHT[]     = "1650";
+const char PROPO_CAR_COMMAND_ENGINE_OFF[]    = "1800";
+const char PROPO_CAR_COMMAND_ENGINE_ON[]     = "1950";
+const char PROPO_CAR_COMMAND_HORN[]          = "1050";
+const char PROPO_CAR_COMMAND_EPA[]           = "1450";
+
+typedef struct {
+     Client *client;
+     Car_Config config;
+} Car;
+
+PROPODEF Car car_create(Client *client, const Car_Config config)
+{
+     Car car = {0};
+     car.client = client;
+     car.config = config;
+     return car;
+}
+
+static int car_command(const Car *car, const char *command)
+{
+     Buffer buffer = buf_from_cstr("ch0 ");
+     buf_append_cstr(&buffer, command);
+     return client_send(car->client, &buffer);
+}
+
+PROPODEF int car_L(const Car *car)
+{
+     return car_command(car, PROPO_CAR_COMMAND_L);
+}
+
+PROPODEF int car_R(const Car *car)
+{
+     return car_command(car, PROPO_CAR_COMMAND_R);
+}
+
+PROPODEF int car_lightoff(const Car *car)
+{
+     return car_command(car, PROPO_CAR_COMMAND_LIGHTOFF);
+}
+
+PROPODEF int car_cabinlight(const Car *car)
+{
+     return car_command(car, PROPO_CAR_COMMAND_CABINLIGHT);
+}
+
+PROPODEF int car_headlight(const Car *car)
+{
+     return car_command(car, PROPO_CAR_COMMAND_HEADLIGHT);
+}
+
+PROPODEF int car_engine_off(const Car *car)
+{
+     return car_command(car, PROPO_CAR_COMMAND_ENGINE_OFF);
+}
+
+PROPODEF int car_engine_on(const Car *car)
+{
+     return car_command(car, PROPO_CAR_COMMAND_ENGINE_ON);
+}
+
+PROPODEF int car_horn(const Car *car)
+{
+     return car_command(car, PROPO_CAR_COMMAND_HORN);
+}
+
+PROPODEF int car_epa(const Car *car)
+{
+     return car_command(car, PROPO_CAR_COMMAND_EPA);
+}
+
+PROPODEF int car_trim(const Car *car, int value)
+{
+     int trim_value = propo_max(-500, propo_min(value, 500)) + PROPO_CAR_NEUTRAL_VALUE;
+
+     Buffer buffer = buf_from_cstr("ch1 ");
+     buf_append_int(&buffer, trim_value);
+     return client_send(car->client, &buffer);
+}
+
+PROPODEF int car_steer(const Car *car, int value)
+{
+     int steering_value = propo_max(-car->config.steering_limits[car->config.steering_limits_idx], propo_min(value, car->config.steering_limits[car->config.steering_limits_idx]));
+     if (car->config.steering_reverse) {
+          steering_value = -steering_value;
+     }
+     steering_value += PROPO_CAR_NEUTRAL_VALUE;
+
+     Buffer buffer = buf_from_cstr("ch2 ");
+     buf_append_int(&buffer, steering_value);
+     return client_send(car->client, &buffer);
+}
+
+PROPODEF int car_throttle(const Car *car, int value)
+{
+     int throttle_value = propo_max(-car->config.throttle_limits[car->config.throttle_limits_idx], propo_min(value, car->config.throttle_limits[car->config.throttle_limits_idx]));
+     if (car->config.throttle_reverse) {
+          throttle_value = -throttle_value;
+     }
+     throttle_value += PROPO_CAR_NEUTRAL_VALUE;
+
+     Buffer buffer = buf_from_cstr("ch3 ");
+     buf_append_int(&buffer, throttle_value);
+     return client_send(car->client, &buffer);
 }
 
 #endif // PROPOTYPE_IMPLEMENTATION
